e5b1601367d294962a021402732ee84bd33cd01e
romanqc <35636603+romanqc@users.noreply.github.com>
2023-07-11 14:26:09 -0400

    Update palm_chat.py

--- palm_chat.py
+++ palm_chat.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 import google.generativeai as palm
-import bot
+from pymongo import MongoClient
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -14,69 +14,86 @@ class palmChat:
     prompt_number = 0
     catalog = {}
 
-    def __init__(self, temp=0.7, status=False, user_input=''):
+    def __init__(self, temp, status, user_input):
         self.temp = temp
         self.status = status
         self.user_input = user_input
+
+        self.examples = [
+                ("What's up?", "I'm great, how may I help you?"),
+                ("Hi", "Hey! How can I help you."),
+                ("Hey", "Hi there, how may I assist you?"),
+                ("Thank you", "You're welcome!")
+            ]
+        
+        self.completion = palm.generate_text(
+                        model=model_id,
+                        prompt=self.get_history() + self.get_user_message(),
+                        temperature=self.get_temperature(),
+                        max_output_tokens=1000,
+                        candidate_count=1
+                    )
+        
+        self.palm_response = self.get_palm_response()
+        self.update_catalog()
+        self.update_prompt_number()
+        self.get_current_conversation()
+
+        print(self.get_current_catalog())
 
     def get_status(self):
         return self.status
         
     def get_temperature(self):
         return self.temp
-        
-    # user_input is type string
-    def start_chat(self):
-        # print("user_input ", self.user_input)
-        # if palmChat.listen() == True:
-        # return b
-        while bot.go_online():
-            if len(palmChat.catalog) < 1:
-                history = ""
-            else:
-                history = palmChat.catalog['convo0']['bot']
-
-            # user input is reset every time a user types
-
-            examples = [
-                ("What's up?", "I'm great, how may I help you?"),
-                ("Hi", "Hey! How can I help you."),
-                ("Hey", "Hi there, how may I assist you?"),
-                ("Thank you", "You're welcome!")
-            ]
-            
-            completion = palm.generate_text(
-                        model=model_id,
-                        prompt=history + self.user_input,
-                        temperature=self.temp,
-                        max_output_tokens=500,
-                        candidate_count=1
-                    )
-
-            response = palm.chat(
-                context="Be an informative assistant that is clear and concise. Be straight to the point, and dont over explain things.",
-                examples=examples,
-                messages=history + self.user_input
-            )
-            
-            bot_output = response.candidates[0]['content']
-
-            convo_number = str(f"convo{palmChat.prompt_number}")
-            palmChat.catalog[convo_number] = {'user': '', 'bot': ''}
-            palmChat.catalog[convo_number]['user'] = self.user_input
-            palmChat.catalog[convo_number]['bot'] = bot_output
-
-            palmChat.prompt_number += 1
-            return bot_output
-
-    # def __str__(self):
-    #     print("string method ",str(self.status))
-    #     return self.status
     
-# a = palmChat(0.4, False)
-# print('temperature = ', a.get_temperature())
-# a.set_status(True)
-# print('getting the current status: ', a.get_status())
+    def get_user_message(self):
+        return self.user_input
+    
+    def get_history(self):
+        if len(palmChat.catalog) < 1:
+            history = ""
+        else:
+            history = palmChat.catalog['convo_0']['bot']
+        return history
+    
+    def get_palm_response(self):
+        response = palm.chat(
+                context="Be an informative assistant that is clear and concise. Be straight to the point, and dont over explain things.",
+                examples=self.examples,
+                messages=self.get_history() + self.get_user_message()
+            )
+        bot_output = response.candidates[0]['content']
+        return bot_output
+    
+    def get_current_catalog(self):
+        return palmChat.catalog
+    
+    def get_current_conversation(self):
+        # print("current catalog ", self.get_current_catalog())
+        # print("current index ", palmChat.prompt_number)
+        current_convo_key = str(f"convo_{palmChat.prompt_number -1}")
+        # print("current convo value ",palmChat.catalog[current_convo_key])
+        current_conversation = {current_convo_key: palmChat.catalog[current_convo_key]}
+        # print("current convo ", current_conversation)
+        return(current_conversation)
 
-# print('setting the status to: ', a.set_status(True))
-# print(a.start_chat('list 5 fruits. limit your response to 5 words'))
+    def create_new_conversation(self):
+        conversation = {'user': '', 'bot': ''}
+        conversation['user'] = self.get_user_message()
+        conversation['bot'] = self.palm_response
+        return conversation
+    
+    def conversation_name(self):
+        number = str(f"convo_{palmChat.prompt_number}")
+        return number
+    
+    def update_prompt_number(self):
+        palmChat.prompt_number += 1
+    
+    def update_catalog(self):
+        self.get_current_catalog()[self.conversation_name()] = self.create_new_conversation()
+        # print('updated, ', self.get_current_catalog())
+
+
+    

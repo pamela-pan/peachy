38608088ccdaec1d14d66c0a6fcdff7f3682fef6
romanqc <35636603+romanqc@users.noreply.github.com>
2023-08-18 14:12:54 -0400

    Updated palm.py
    added previous dialogue
        the bot is now context aware of the user previous conversation

--- palm.py
+++ palm.py
@@ -18,26 +18,31 @@ class palmChat():
         self.temp = temp
         self.user_input = user_input
         self.examples = [
-            ('hi', 'Hi, how may I help you?'),
-            ('hey', 'Hi, how can I assist you?')
+            ("This is context from a previous conversation: (user:'list five fruits', bot:'apple, banana, mango, pineapple, orange'). Use this context to help your response to the following message if it applies: what is the first item in the list?", "Apple"),
+            ("This is context from a previous conversation: (user:'', bot:''). Use this context to help your response to the following message if it applies: what is the first item in the list?", "Sorry, I didn't get that. Would you like me to list some items for you?"),
+            ("This is context from a previous conversation: (user:'what is the first item in the list?', bot:'Sorry, I didn't get that. Would you like me to list some items for you?'). Use this context to help your response to the following message if it applies: yes", "Sure, here is a random list of five countries: Guadamala, New Zealand, Australia, Greenland, Canada"),
+            ("This is context from a previous conversation: (user:'56+44', bot:'100'). Use this context to help your response to the following message if applies: my bad, i didnt mean that, i meant to say minus", "No worries, 56 - 44 = 12")
         ]
         self.response = self.palm_response()
 
     def palm_response(self):
-        if self.user_input == 'create a new catalog':
-            output = 'A new catalog has been created.'
-            self.update_catalogs()
-            return output
-        else:
             response = palm.chat(
-                context='Informative Assistant',
+                context='Be an informative assistant, be direct, not too friendly but still helpful.',
                 examples=self.examples,
-                messages=self.user_input,
-                temperature=self.temp
+                messages=str(self.previous_dialouge()+self.user_input),
+                temperature=self.temp,
             )
             output = response.candidates[0]['content']
             self.update_conversations(output)
             return output
+        
+    def previous_dialouge(self):
+        document = DocumentsHandler.get_document_by_user(self.user_id)
+        if len(document['catalogs'][-1]['conversation']) < 1:
+            context = '(user:'', bot:'')'
+        else: 
+            context = str("This is context from a previous conversation: " + f"(user:'{document['catalogs'][-1]['conversation'][-1]['user']}', bot:'{document['catalogs'][-1]['conversation'][-1]['bot']}'). " + "Use this context to help responond to the following message if you think it applies: ")
+        return context
 
     def update_conversations(self, response):
         document = DocumentsHandler.get_document_by_user(self.user_id)
